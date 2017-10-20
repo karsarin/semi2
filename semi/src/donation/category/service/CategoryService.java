2d1a79533c62d544ddefe10095fc163e57dec420
kailrera <kailrera@naver.com>
2017-10-20 09:58:59 +0900

    CategoryService 작업

--- semi/src/donation/category/service/CategoryService.java
+++ semi/src/donation/category/service/CategoryService.java
@@ -1,5 +1,19 @@
 package donation.category.service;
 
+import java.util.ArrayList;
+
+import donation.category.vo.Category;
+
 public class CategoryService {
+
+	public int getListCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public ArrayList<Category> selectList(int currentPage, int limit) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
