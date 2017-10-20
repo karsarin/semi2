5d824334ec733ab18a0d11b266dc8b72c11a8073
hansubin <alupin813@naver.com>
2017-10-20 09:58:10 +0900

    충돌 수정

--- semi/src/donation/category/controller/CategoryListViewServlet.java
+++ semi/src/donation/category/controller/CategoryListViewServlet.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class CategoryListView
  */
 @WebServlet("/clist")
-public class CategoryListView extends HttpServlet {
+public class CategoryListViewServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public CategoryListView() {
+    public CategoryListViewServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
