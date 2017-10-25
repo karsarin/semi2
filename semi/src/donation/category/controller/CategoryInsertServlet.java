7bcacda11e125fa6f6c908ede911259c2c28b8e8
kailrera <kailrera@naver.com>
2017-10-25 15:06:06 +0900

    충돌 수정

--- semi/src/donation/category/controller/CategoryInsertServlet.java
+++ semi/src/donation/category/controller/CategoryInsertServlet.java
@@ -60,19 +60,64 @@ public class CategoryInsertServlet extends HttpServlet {
 		// 해당 컨테이너에서 구동중인 웹 애플리케이션의 루트 경로 알아냄
 		String root = request.getSession().getServletContext().getRealPath("/");
 		// 업로드 되는 파일이 저장을 폴더명과 루트 경로 연결 처리
-		String savePath = root + "buploadfiles";
+		String savePath = root + "cuploadfiles";
 		// web/uploadfiles 로 지정됨
 		
 		// request를 MultipartRequest 객체로 변환함
 		// 자동 지정된 경로에 파일 저장됨
 		MultipartRequest mrequest = new MultipartRequest(request, savePath, maxSize, "utf-8", new DefaultFileRenamePolicy());
 		
-		String title = mrequest.getParameter("btitle");
-		String writer = mrequest.getParameter("bwriter");
-		String content = mrequest.getParameter("bcontent");
+		String title = mrequest.getParameter("ctitle");
+		String writer = "user1010";
+		String content = mrequest.getParameter("ccontent");
+		int donation = Integer.parseInt(mrequest.getParameter("cdonation"));
+		int workdate = Integer.parseInt(mrequest.getParameter("cworkdate"));
+		String email = mrequest.getParameter("cemail");
+		String phone = mrequest.getParameter("cphone");
+		int group = 1;
 		
+		/*switch(mrequest.getParameter("category"))
+		{
+		case "IT":
+			group = 1;
+			break;
+		case "디자인":
+			group = 2;
+			break;
+		case "마케팅":
+			group = 3;
+			break;
+		case "음악":
+			group = 4;
+			break;
+		case "노하우/상담":
+			group = 5;
+			break;
+		case "번역":
+			group = 6;
+			break;
+		case "건강":
+			group = 7;
+			break;
+		case "기타":
+			group = 8;
+			break;
+				
+		}*/
 		String originalFileName = mrequest.getFilesystemName("upfile");
 		Category c = null;
+		
+		System.out.println("group = " + group);
+		System.out.println(mrequest.getParameter("IT"));
+		
+		System.out.println("title : " + title);
+		System.out.println("writer : " + writer);
+		System.out.println("content : " + content);
+		System.out.println("donation : " + donation);
+		System.out.println("workdate : " + workdate);
+		System.out.println("email : " + email);
+		System.out.println("phone : " + phone);
+		System.out.println("originalFileName :" + originalFileName);
 		
 		if(originalFileName != null)
 		{
@@ -105,16 +150,17 @@ public class CategoryInsertServlet extends HttpServlet {
 				originalFile.delete();
 			}
 			
-			c = new Category(title, writer, content, originalFileName, renameFileName);
+			c = new Category(writer, title, group, writer, content, donation, workdate, email, phone, originalFileName, renameFileName);
+		} else {
+			c = new Category(writer, title, group, writer, content, donation, workdate, email, phone, null, null);
 		}
-			c = new Category(title, writer, content, null, null);
 		
 		if(new CategoryService().insertCategory(c) > 0)
 		{
 			response.sendRedirect("/semi/clist?page=1");
 		} else {
-			view = request.getRequestDispatcher("views/category/categoryError.jps");
-			request.setAttribute("message", "category 서비스 등록 실패");
+			view = request.getRequestDispatcher("views/category/categoryError.jsp");
+			request.setAttribute("message", "category 등록 실패");
 			view.forward(request, response);
 		}
 	}
